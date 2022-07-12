3d6c57b2998de122e92bf55fc0b70c1d63ea2f6c
Pius Kariuki <piuskariuki19@gmail.com>
2022-07-12 17:14:48 +0300

    first

---
+++ ActionTypes.js
@@ -0,0 +1,3 @@
+export const addToCart = "ADDTOCART"
+export const removeFromCart = "REMOVE"
+export const clearCart = "CLEARCART"
---
+++ index.js
@@ -0,0 +1,70 @@
+const addToCart = "ADDTOCART";
+const removeFromCart = "REMOVE";
+const clearCart = "CLEARCART";
+
+const initialState = [];
+
+const reducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case addToCart: {
+      const { name, price, id, quantity } = payload;
+      if (state && state.find((item) => item.id === id)) {
+        return state.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + quantity };
+          }
+          return item;
+        });
+      }
+      return [...state, payload];
+    }
+
+    case removeFromCart: {
+      const id = payload;
+      return state.filter((item) => item.id !== id);
+    }
+
+    case clearCart:
+      () => initialState;
+
+    default:
+      return state;
+  }
+};
+
+
+const persistedState = () =>
+  localStorage.getItem("generic-redux-cart") !== null
+    ? localStorage.getItem("generic-redux-cart")
+    : [];
+
+const store = Redux.createStore(reducer, persistedState());
+
+
+export const state = store.getState();
+export const subscribe = store.subscribe(() => {
+  localStorage.setItem("generic-redux-cart", store.getState());
+  return store.getState();
+});
+
+export const addItem = (item) => {
+  store.dispatch({ type: addToCart, payload: item });
+};
+
+export const deleteItem = (id) => {
+  store.dispatch({ type: removeFromCart, payload: id });
+};
+
+export const clear = () => {
+  store.dispatch({ type: clearCart });
+};
+
+// const newItem = {
+//   name: "Mazda cx9",
+//   price: 240000,
+//   quantity: 1,
+//   id: 8,
+// };
+
+// deleteItem(5);
+// addItem(newItem);
